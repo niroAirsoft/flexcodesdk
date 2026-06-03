b043b4ed32a7aa419779e320635c747667829411
niroAirsoft <116628080+niroAirsoft@users.noreply.github.com>
2026-06-03 09:24:04 +0800

    Update config/flexcodesdk.php to fix Laravel 9 incompatibilities

--- config/flexcodesdk.php
+++ config/flexcodesdk.php
@@ -15,4 +15,15 @@ return [
             'vkey' 	=> env('FLEXCODE_VKEY'),
         ),
     ),
+
+    'fingerprint_storage' => array(
+        'table' => env('FLEXCODE_FINGERPRINT_TABLE', 'demo_finger'),
+        'user_column' => env('FLEXCODE_FINGERPRINT_USER_COLUMN', 'user_id'),
+        'data_column' => env('FLEXCODE_FINGERPRINT_DATA_COLUMN', 'finger_data'),
+        'verify_column' => env('FLEXCODE_FINGERPRINT_VERIFY_COLUMN', 'verify'),
+        'deleted_at_column' => env('FLEXCODE_FINGERPRINT_DELETED_AT_COLUMN', 'deleted_at'),
+    ),
+
+    'redirect_after_register' => env('FLEXCODE_REDIRECT_AFTER_REGISTER', url('/')),
+    'redirect_after_verify' => env('FLEXCODE_REDIRECT_AFTER_VERIFY', url('/')),
 ];
